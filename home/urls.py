d6ea25862ce02e4387d9fe7a18a672035e565f9c
SONALI TUKARAM RATHOD CB ADMIN OFFICE <sonali.rathod@adl.christuniversity.in>
2022-09-24 15:53:36 +0530

    Our service page is updated and product page is added

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
-from home.views import index, internship, internshipForm, serviceForm
+
+from home.views import index, internship, internshipForm, serviceForm, ourProduct
 
 urlpatterns = [
     path('', index, name='main'),
     path('internship',internship,name='internship'),
     path('internshipForm',internshipForm,name='internshipForm'),
     path('serviceForm',serviceForm,name='serviceForm'),
+    path('ourProduct',ourProduct,name='ourProduct'),
+    
 ]
--- home/views.py
+++ home/views.py
@@ -6,15 +6,17 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'home/index.html')
 
-
 def internship(request):
     return render(request,'internship/internship.html')
-
 
 def internshipForm(request):
     return render(request,'support/internshipForm.html')
 
-
 def serviceForm(request):
     return render(request,'support/service.html')
+
+def ourProduct(request):
+    return render(request,'products/products.html')
+
+
 
