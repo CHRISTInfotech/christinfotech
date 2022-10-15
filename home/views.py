8e16d1f16839368dab4feb788ed988b38788112f
SONALI TUKARAM RATHOD CB ADMIN OFFICE <sonali.rathod@adl.christuniversity.in>
2022-10-15 13:43:48 +0530

    Index page is updated and Internshipform data is now storing in database

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from home.models import Internship
 # Create your views here.
 
 
@@ -10,7 +10,23 @@ def internship(request):
     return render(request,'internship/internship.html')
 
 def internshipForm(request):
-    return render(request,'support/internshipForm.html')
+    if request.method == "POST":
+        name=request.POST['name']
+        email=request.POST['email']
+        phoneno=request.POST['phoneno']
+        dob=request.POST['dob']
+        institute=request.POST['institute']
+        course=request.POST['course']
+        areaofinterest=request.POST['areaofinterest']
+        hours=request.POST['hours']
+        resume=request.FILES['resume']
+
+        object=Internship.objects.create(name=name,email=email,phoneno=phoneno,dob=dob,institute=institute,course=course,areaofinterest=areaofinterest,hours=hours,resume=resume)
+        object.save()  
+        return redirect('internshipForm')
+    else :
+        return render(request,'support/internshipForm.html')
+
 
 def serviceForm(request):
     return render(request,'support/service.html')
