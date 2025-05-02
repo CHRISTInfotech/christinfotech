7d773e60f548913797c1dfb39b359c81c74c345d
alansomathew <alansomathew10@gmail.com>
2025-05-02 10:24:22 +0530

    changes

--- adminPanel/admin.py
+++ adminPanel/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
+from .models import Internship, Development, Newsletter
 
-# Register your models here.
-# from adminPanel.models import Internship
+@admin.register(Internship)
+class InternshipAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phoneno', 'institute', 'course', 'areaofinterest', 'hours', 'request_date')
+    search_fields = ('name', 'email', 'phoneno', 'institute', 'course', 'areaofinterest')
+    list_filter = ('course', 'areaofinterest', 'request_date')
+    ordering = ('-request_date',)
+    date_hierarchy = 'request_date'
 
-# admin.site.register(Internship)
+@admin.register(Development)
+class DevelopmentAdmin(admin.ModelAdmin):
+    list_display = ('fname', 'lname', 'email', 'phoneno', 'compinst', 'department', 'request_date')
+    search_fields = ('fname', 'lname', 'email', 'phoneno', 'compinst', 'department')
+    list_filter = ('department', 'request_date')
+    ordering = ('-request_date',)
+    date_hierarchy = 'request_date'
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('email_sub',)
+    search_fields = ('email_sub',)
+    list_filter = ('email_sub',)
+    ordering = ('-id',)
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from adminPanel.models import Internship, Development, Newsletter
 from django.contrib import messages
 from datetime import datetime
@@ -8,98 +8,88 @@ from datetime import datetime
 
 def index(request):
     if request.method == "POST":
-        email_sub=request.POST['email'] 
-        obj=Newsletter.objects.filter(email_sub=email_sub).count()
-        if obj >= 1:
-            messages.success(request, "You're already subscribed. Thank You!")    
-            return redirect('main')
+        email_sub = request.POST.get('email')
+        if email_sub:
+            obj, created = Newsletter.objects.get_or_create(email_sub=email_sub)
+            if not created:
+                messages.info(request, "You're already subscribed. Thank You!")
+            else:
+                messages.success(request, "Thank you for subscribing to our newsletter!")
         else:
-            Newsletter.objects.create(email_sub=email_sub)    
-            messages.success(request, "Thank You for subscribing to our newsletter!")    
-            return redirect('main')
-    else:
-        return render(request, 'home/index.html')
+            messages.warning(request, "Please enter a valid email address.")
+        return redirect('main')
+    return render(request, 'home/index.html')
 
 def internship(request):
-    return render(request,'internship/internship.html')
+    return render(request, 'internship/internship.html')
+
 
 def internshipForm(request):
-    if request.method == "POST" and request.FILES:
-        name=request.POST['name']
-        email=request.POST['email']
-        phoneno=request.POST['phoneno']
-        dob=request.POST['dob']
-        formatted_date = datetime.strptime(dob, '%d-%m-%Y').strftime('%Y-%m-%d')
-        institute=request.POST['institute']
-        course=request.POST['course']
-        areaofinterest=request.POST['areaofinterest']
-        hours=request.POST['hours']        
-        resume=request.FILES['resume']
-        object=Internship.objects.create(name=name,email=email,phoneno=phoneno,dob=formatted_date,institute=institute,course=course,areaofinterest=areaofinterest,hours=hours,resume=resume)
-        object.save()  
-        messages.success(request, "Request received, will get back to you soon. ")
-        return redirect('internshipForm')
-    
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phoneno = request.POST.get('phoneno')
+        dob = request.POST.get('dob')
+        institute = request.POST.get('institute')
+        course = request.POST.get('course')
+        areaofinterest = request.POST.get('areaofinterest')
+        hours = request.POST.get('hours')
+        resume = request.FILES.get('resume')  # Will be None if not uploaded
 
-    elif request.method == "POST":
-            name=request.POST['name']
-            email=request.POST['email']
-            phoneno=request.POST['phoneno']
-            dob=request.POST['dob']
-            formatted_date = datetime.strptime(dob, '%d-%m-%Y').strftime('%Y-%m-%d')
-            # print(formatted_date)
-            institute=request.POST['institute']
-            course=request.POST['course']
-            areaofinterest=request.POST['areaofinterest']
-            hours=request.POST['hours']        
-
-            object=Internship.objects.create(name=name,email=email,phoneno=phoneno,dob=formatted_date,institute=institute,course=course,areaofinterest=areaofinterest,hours=hours)
-            object.save()  
-            messages.success(request, "Request received, will get back to you soon. ")
-            # messages.error(request, "Error: This is the sample error Flash message.")
-            # post.save()
+        try:
+            formatted_dob = datetime.strptime(dob, '%d-%m-%Y').strftime('%Y-%m-%d')
+        except ValueError:
+            messages.error(request, "Invalid date format. Please use DD-MM-YYYY.")
             return redirect('internshipForm')
-    else :
-        return render(request,'support/internshipForm.html')
-    
+
+        internship_obj = Internship.objects.create(
+            name=name,
+            email=email,
+            phoneno=phoneno,
+            dob=formatted_dob,
+            institute=institute,
+            course=course,
+            areaofinterest=areaofinterest,
+            hours=hours,
+            resume=resume if resume else None
+        )
+
+        messages.success(request, "Request received, we'll get back to you soon.")
+        return redirect('internshipForm')
+
+    return render(request, 'support/internshipForm.html')
 
 
 def serviceForm(request):
-    if request.method == "POST" and request.FILES:
-        fname=request.POST['fname']
-        lname=request.POST['lname']
-        phoneno=request.POST['phoneno']
-        email=request.POST['email']
-        compinst=request.POST['compinst']
-        department=request.POST['department']
-        requestdesc=request.POST['requestdesc']
-        document=request.FILES['document']
+    if request.method == "POST":
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        phoneno = request.POST.get('phoneno')
+        email = request.POST.get('email')
+        compinst = request.POST.get('compinst')
+        department = request.POST.get('department')
+        requestdesc = request.POST.get('requestdesc')
+        document = request.FILES.get('document')  # Can be None
 
-        object=Development.objects.create(fname=fname,lname=lname,phoneno=phoneno,email=email,compinst=compinst,department=department,requestdesc=requestdesc,document=document)
-        object.save()
-        messages.success(request, "Request received, will get back to you soon. ")
+        Development.objects.create(
+            fname=fname,
+            lname=lname,
+            phoneno=phoneno,
+            email=email,
+            compinst=compinst,
+            department=department,
+            requestdesc=requestdesc,
+            document=document if document else None
+        )
+
+        messages.success(request, "Request received, we'll get back to you soon.")
         return redirect('serviceForm')
-    
-    elif request.method == "POST":
-            fname=request.POST['fname']
-            lname=request.POST['lname']
-            phoneno=request.POST['phoneno']
-            email=request.POST['email']
-            compinst=request.POST['compinst']
-            department=request.POST['department']
-            requestdesc=request.POST['requestdesc']
 
-            object=Development.objects.create(fname=fname,lname=lname,phoneno=phoneno,email=email,compinst=compinst,department=department,requestdesc=requestdesc)
-            object.save()
-            messages.success(request, "Request received, will get back to you soon. ")
-            return redirect('serviceForm')
-    else :
-        return render(request,'support/service.html')
+    return render(request, 'support/service.html')
 
 def ourProduct(request):
-    return render(request,'products/products.html')
-    
-def ourTeam(request):
-    return render(request,'aboutus/aboutus.html')
+    return render(request, 'products/products.html')
 
-    
+
+def ourTeam(request):
+    return render(request, 'aboutus/aboutus.html')
